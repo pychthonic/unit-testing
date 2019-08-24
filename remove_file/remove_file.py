231ec451757d908bed749825eee72c7fbd49fc30
pychthonic <fridgewolf@protonmail.com>
2019-08-24 13:30:02 -0400

    pep8ified it cause why not

--- remove_file/remove_file.py
+++ remove_file/remove_file.py
@@ -1,7 +1,13 @@
+"""This is a basic file with a function to test. The file
+takes a filename and deletes it.
+"""
+
 import os
 
 def delete_file(filename):
+    """Deletes filename."""
     os.remove(filename)
+
 
 if __name__ == "__main__":
     delete_file('test_file.txt')
--- remove_file/test_remove_file.py
+++ remove_file/test_remove_file.py
@@ -1,15 +1,27 @@
+"""This is a unit test script to test remove_file.py
+It creates a file called test_file, tests that it exists,
+then deletes it, and tests that it has been deleted."""
+
 import unittest
 import os
 import remove_file
-import tempfile
 
 
 class TestRemove(unittest.TestCase):
+    """The setUp method creates test_file and writes
+    a string to it. The test_remove method tests that
+    the file exists, removes it, then tests that it's
+    gone.
+    """
+
     def setUp(self):
-        with open('test_file', 'w') as f:
-            f.write('this is a test')
+        with open('test_file', 'w') as file_descriptor:
+            file_descriptor.write('this is a test')
 
     def test_remove(self):
+        """Tests that file exists, deletes file, tests
+        that file has been deleted.
+        """
         self.assertTrue(os.path.exists('test_file'))
         remove_file.delete_file('test_file')
         self.assertFalse(os.path.exists('test_file'))
